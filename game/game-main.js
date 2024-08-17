ab481ec1a41b3d92b4e0a4af11498b2babfc7fd4
Danial Jivraj <danialjivraj@gmail.com>
2024-08-17 20:59:28 +0100

    small update

--- game/game-main.js
+++ game/game-main.js
@@ -164,7 +164,7 @@ window.addEventListener('load', function () {
             this.enterToTalkToPenguin = false;
             this.talkToPenguinOneTimeOnly = true;
             // elyvorg
-            this.maxCoinsToFightElyvorg = 70;
+            this.maxCoinsToFightElyvorg = 100;
             this.isElyvorgFullyVisible = false;
             this.elyvorgSpawned = false;
             this.elyvorgDialogueBeforeDialoguePlayOnce = true;
