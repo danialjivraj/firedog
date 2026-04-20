6006056ee83e32db6de62731b13d1d79f6729ada
danialjivraj <danialjivraj@gmail.com>
2026-04-20 01:45:58 +0100

    for display window menu

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    quitApp: () => ipcRenderer.send('quit-app')
+    quitApp: () => ipcRenderer.send('quit-app'),
+    setWindowMode: (mode) => ipcRenderer.send('set-window-mode', mode)
 });
